477660f0309626f3022c8f297ee3fb3073619efa
Maxzyto <adaaziabugrimaxwell@gmail.com>
2025-01-24 04:28:44 +0000

    fatch data file added

---
+++ fetch-data.js
@@ -0,0 +1,29 @@
+async function fetchUserData(userID) {
+  const apiUrl = "https://jsonplaceholder.typicode.com/users";
+  const dataContainer = document.getElementById("api-data");
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    if (dataContainer) {
+      dataContainer.innerHTML = ``;
+
+      const userList = document.createElement("ul");
+
+      data.forEach((user) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = user.name;
+        userList.appendChild(listItem);
+      });
+
+      dataContainer.appendChild(userList);
+    }
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchUserData(1);
+});
